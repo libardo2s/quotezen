2bc56d874f5581891bc7ee4555ee581a65284e42
kandreyrosales <kandreyrosales@gmail.com>
2025-04-08 10:43:47 -0500

    Pending quotes and pending carrier quotes

--- app/run.py
+++ app/run.py
@@ -1,10 +1,10 @@
 from flask import Flask
-from app.config import Config
-from app.database import db
+from config import Config
+from database import db
 from flask_migrate import Migrate
 
 # Import Blueprint for Routes
-from app.routes import app_routes
+from routes import app_routes
 
 # Initialize Flask App
 app = Flask(__name__)
